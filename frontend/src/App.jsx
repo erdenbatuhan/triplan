58fca3ca56747f1c28f177fc44f5d189a3b2b2c3
anilkul98 <akul16@ku.edu.tr>
2022-06-10 17:45:11 +0200

    Delete unnecessary comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
-// import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import './App.css';
 import TripPlanningLayout from './components/trip-planning-pagelayout';
-// import TripPlanningCard from './components/card-item';
-// import TripPlanningList from './components/list';
 
 function App() {
   return (
     <div className="App">
-      {/* <TripPlanningCard
-        content="Good place to chill in summer."
-        title="English Garden"
-        type="place"
-      /> */}
       <TripPlanningLayout />
     </div>
   );
