b8b33046233630c6a3264a334df761492bcc9c5d
EralpKocas <hkocas15@ku.edu.tr>
2022-06-18 01:39:37 +0300

    creating user query is implemented.

--- frontend/src/queries/user-queries.jsx
+++ frontend/src/queries/user-queries.jsx
@@ -1,4 +1,4 @@
-export default async function getUserData() {
+export async function getUserData() {
   const url = `http://localhost:8008/users`;
 
   const allUserData = await fetch(url, {
@@ -13,4 +13,22 @@ export default async function getUserData() {
     .then(async (response) => response.json())
     .then((json) => json);
   return allUserData;
+}
+
+export async function createNewUser(userData) {
+  const url = `http://localhost:8008/users`;
+
+  const createData = await fetch(url, {
+    method: `POST`,
+    mode: `cors`,
+    headers: {
+      'Access-Control-Allow-Origin': `http://localhost:3000`,
+      'Access-Control-Allow-Credentials': `true`,
+      'Content-Type': `application/json; charset=UTF-8`
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(async (response) => response.json())
+    .then((json) => json);
+  return createData;
 }
