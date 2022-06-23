d1219bfddc3d2b07d190f502c705ff96022bd028
Batuhan Erden <erden.batuhan@gmail.com>
2022-06-23 22:58:01 +0200

    update user service to include the getting one user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,35 @@
 const { User } = require("./../models/user.js");
 
-const findAll = (req, res) => {
-  User.find()
-    .sort({ createdAt: -1 }) // In descending order/newly created first
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      res
-        .status(400)
-        .send(
-          `An error occurred while getting all the records from User collection`
-        );
-    });
+const findAll = async (req, res) => {
+  try {
+    const users = await User.find().sort({ createdAt: -1 }) // In descending order/newly created first
+    res.status(200).send(users);
+  } catch ({ message }) {
+    res.status(400).send(`An error occurred while getting all the users! Error => ${message}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const user = await User.findById(userId)
+
+    if (user) {
+      res.status(200).send(user)
+    } else {
+      res.status(404).send(`No user found with ID=${userId}`)
+    }
+  } catch ({ message }) {
+    res.status(400).send(`An error occurred while getting the user! Error => ${message}`);
+  }
 };
 
 const createUser = (req, res) => {
-  const newUser = new User(req.body);
-  newUser
-    .save()
-    .then((result) => {
-      res.status(201).send(result);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    new User(req.body).save().then(result => res.status(201).send(result))
+  } catch ({ message }) {
+    res.status(400).send(`An error occurred while creating the user! Error => ${message}`);
+  }
 };
 
-module.exports = { findAll, createUser };
+module.exports = { findAll, findOne, createUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const userController = require("./../controllers/userController.js");
 
 const router = express.Router();
+
 router.get("/", userController.findAll);
+router.get("/:id", userController.findOne);
 router.post("/", userController.createUser);
 
 module.exports = router;
