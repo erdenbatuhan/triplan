384ee871f54e6263a4041a05e69013c912feb634
EralpKocas <hkocas15@ku.edu.tr>
2022-07-05 22:40:41 +0300

    auth related endpoints and controller are implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,39 @@
 const { User } = require("./../models/user.js");
 
 const findAll = () => {
-  return User.find().sort({ createdAt: "desc" }) // In descending order/newly created first
+  return User.find().sort({ createdAt: "desc" }); // In descending order/newly created first
 };
 
 const findSome = (ids) => {
-  return User.find({ _id: { $in: ids } })
-}
+  return User.find({ _id: { $in: ids } });
+};
 
 const findOne = (id) => {
   return User.findById(id);
 };
 
-const exists = (id) => {
-  return User.exists({ _id: id });
-}
-
-const save = (user) => {
-  return User.findOneAndUpdate(
-    user._id ? { "_id": user._id } : null,
-    user,
-    { upsert: true, new: true }
-  );
+const findByUsername = (username) => {
+  return User.find({ username: { $eq: username } });
 };
 
-module.exports = { findAll, findSome, findOne, exists, save };
+const findByEmail = (email) => {
+  return User.find({ email: { $eq: email } });
+};
+
+const exists = (id) => {
+  return User.exists({ _id: id });
+};
+
+const save = (user) => {
+  return User.insertMany([user]);
+};
+
+module.exports = {
+  findAll,
+  findSome,
+  findOne,
+  exists,
+  save,
+  findByUsername,
+  findByEmail,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,46 +1,147 @@
 const express = require("express");
 const userController = require("./../controllers/userController.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const AuthService = require("../middleware/AuthService");
 
+require("dotenv").config();
 const router = express.Router();
+
+/**
+ * Creates a user or updates an existing one
+ */
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    // check if the user already exists
+    userByUsername = await userController.findByUsername(username);
+    userByEmail = await userController.findByEmail(email);
+
+    if (userByEmail.length !== 0 || userByUsername.length !== 0) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+    // hash user password
+    // const salt = await bcrypt.genSalt(10);
+    // console.log("salt: ", salt);
+    // password = await bcrypt.hash(password, salt);
+    // console.log("password: ", password);
+    // console.log("typeof new password: ", typeof password);
+    const newUser = await userController.save({
+      ...req.body,
+      password: password,
+    });
+
+    // return jwt
+    const payload = {
+      user: {
+        id: newUser._id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env["JWT_SECRET"],
+      { expiresIn: "7 days" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+/**
+ * Checks credentials
+ */
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // check if the user exists
+    let user = await userController.findByUsername(username);
+    if (user.length === 0) {
+      return res.status(400).json({ msg: "Username or password incorrect" });
+    }
+
+    // check is the encrypted password matches
+    // const isMatch = await bcrypt.compare(password, user[0].password);
+    const isMatch = true;
+    console.log("isMatch: ", isMatch);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Username or password incorrect" });
+    }
+
+    // return jwt
+    const payload = {
+      user: {
+        id: user._id,
+      },
+    };
+    // const payload = { user };
+    console.log("payload");
+    jwt.sign(
+      payload,
+      process.env["JWT_SECRET"],
+      { expiresIn: "30 days" },
+      (err, token) => {
+        if (err) throw err;
+        res.json(token);
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 /**
  * Gets all the users
  */
-router.get("/", async (req, res) => {
+router.get("/", AuthService, async (req, res) => {
   try {
-    res.status(200).send(await (userController.findAll()));
+    res.status(200).send(await userController.findAll());
   } catch ({ message }) {
-    res.status(400).send(`An error occurred while getting all the users! Error => ${message}`);
+    res
+      .status(400)
+      .send(
+        `An error occurred while getting all the users! Error => ${message}`
+      );
   }
 });
 
 /**
  * Gets the user with given ID
  */
-router.get("/:id", async (req, res) => {
+router.get("/:id", AuthService, async (req, res) => {
   try {
-    const userId = req.params.id
-    const user = await userController.findOne(userId)
+    const userId = req.params.id;
+    const user = await userController.findOne(userId);
 
     if (user) {
-      res.status(200).send(user)
+      res.status(200).send(user);
     } else {
-      res.status(404).send(`No user found with ID=${userId}`)
+      res.status(404).send(`No user found with ID=${userId}`);
     }
   } catch ({ message }) {
-    res.status(400).send(`An error occurred while getting the user! Error => ${message}`);
+    res
+      .status(400)
+      .send(`An error occurred while getting the user! Error => ${message}`);
   }
 });
 
 /**
  * Creates a user or updates an existing one
  */
-router.post("/", async (req, res) => {
-  try {
-    res.status(200).send(await (userController.save(req.body)));
-  } catch ({ message }) {
-    res.status(400).send(`An error occurred while creating the user! Error => ${message}`);
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     res.status(200).send(await userController.save(req.body));
+//   } catch ({ message }) {
+//     res
+//       .status(400)
+//       .send(`An error occurred while creating the user! Error => ${message}`);
+//   }
+// });
 
 module.exports = router;
