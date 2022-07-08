14cdbffeb872957233d193eda188ba4b419118b0
Cansu Yildirim <cansu.yildirim@tum.de>
2022-07-08 03:54:04 +0200

    backend for updating/creating trip location with rating and comment

--- backend/controllers/tripLocationController.js
+++ backend/controllers/tripLocationController.js
@@ -4,4 +4,28 @@ const findByIds = (ids) => {
   return TripLocation.find({ _id: { $in: ids } }).sort({ createdAt: "desc" }); // In descending order/newly created first
 };
 
-module.exports = { findByIds };
+const create = (fields) => {
+  return new Promise((resolve, reject) => {
+    const tripLocation = TripLocation.create(fields);
+
+    resolve(tripLocation);
+    }).catch(err => reject(err));
+};
+
+const update = (id, fields) => {
+  if (!exists(id)) {
+    return new Promise(resolve => resolve(null)); // Trip Location does not exist!
+  }
+
+  return TripLocation.findOneAndUpdate(
+    { "_id": id },
+    { $set: fields },
+    { new: true }
+  );
+};
+
+const exists = (id) => {
+  return TripLocation.exists({ _id: id });
+};
+
+module.exports = { findByIds, create, update, exists };
