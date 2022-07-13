5dfc7a1f03aef1c9066ee7b18bc8a981683603cd
EralpKocas <hkocas15@ku.edu.tr>
2022-07-14 01:10:42 +0300

    rest of the filter options are added in frontend

--- frontend/src/components/PlaceFilter.jsx
+++ frontend/src/components/PlaceFilter.jsx
@@ -1,30 +1,89 @@
-import React from 'react';
-// import { Box, Card, CardActions, Typography, Grid } from '@mui/material';
-import { Box, Grid, FormGroup, FormControlLabel, Checkbox, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Box,
+  FormGroup,
+  FormControl,
+  // InputLabel,
+  Select,
+  MenuItem,
+  FormControlLabel,
+  Checkbox,
+  Typography
+} from '@mui/material';
 // import PropTypes from 'prop-types';
 
 function PlaceFilter() {
+  const [cuisine, setCuisine] = useState(['']);
+  const [priceLevel, setPriceLevel] = useState(['']);
+
+  const handleCuisineChange = (event) => {
+    setCuisine((cuisines) => [...cuisines, event.target.value]);
+  };
+
+  const handlePriceLevelChange = (event) => {
+    setPriceLevel((priceLevels) => [...priceLevels, event.target.value]);
+  };
+
   return (
     <Box
       sx={{
-        mt: 4,
-        marginLeft: 5,
+        mt: 2,
+        marginLeft: 1,
         marginRight: 5,
-        marginBottom: 5
+        marginBottom: 5,
+        minWidth: 250
       }}>
-      <Grid container direction="column" justifyContent="center" alignItems="center">
-        <Grid item>
-          <Box sx={{ p: 2, borderColor: 'black', border: 2 }}>
-            <Typography align="center">Type of Place(s)</Typography>
-            <FormGroup>
-              <FormControlLabel control={<Checkbox defaultChecked />} label="Museum" />
-              <FormControlLabel control={<Checkbox />} label="Natural Park" />
-              <FormControlLabel control={<Checkbox />} label="Art Gallery" />
-              <FormControlLabel control={<Checkbox />} label="Landmarks" />
-            </FormGroup>
-          </Box>
-        </Grid>
-      </Grid>
+      <Box sx={{ p: 2, borderColor: 'black', border: 2 }}>
+        <Typography align="left">Type of Place(s)</Typography>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Museum" />
+          <FormControlLabel control={<Checkbox />} label="Natural Park" />
+          <FormControlLabel control={<Checkbox />} label="Art Gallery" />
+          <FormControlLabel control={<Checkbox />} label="Landmarks" />
+        </FormGroup>
+      </Box>
+      <Box sx={{ p: 2, borderColor: 'black', border: 2, borderTop: 0 }}>
+        <Typography align="left">Cuisine</Typography>
+        <FormControl align="left" sx={{ minWidth: 120 }} size="small">
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={cuisine}
+            // label=""
+            onChange={handleCuisineChange}>
+            <MenuItem value="">
+              <em>Select Cuisine(s)</em>
+            </MenuItem>
+            <MenuItem value="Japanese">Japanese</MenuItem>
+            <MenuItem value="Chinese">Chinese</MenuItem>
+            <MenuItem value="Indian">Indian</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box sx={{ p: 2, borderColor: 'black', border: 2, borderTop: 0 }}>
+        <Typography align="left">Price Level</Typography>
+        <FormControl align="left" sx={{ minWidth: 120 }} size="small">
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={priceLevel}
+            // label=""
+            onChange={handlePriceLevelChange}>
+            <MenuItem value="">
+              <em>Select Price Level(s)</em>
+            </MenuItem>
+            <MenuItem value="€">€</MenuItem>
+            <MenuItem value="€€">€€</MenuItem>
+            <MenuItem value="€€€">€€€</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box sx={{ p: 2, borderColor: 'black', border: 2, borderTop: 0 }}>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Vegan" />
+          <FormControlLabel control={<Checkbox />} label="Vegetarian" />{' '}
+        </FormGroup>
+      </Box>
     </Box>
   );
 }
