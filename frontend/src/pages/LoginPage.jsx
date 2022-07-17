f364459c63255c4a529b89743a22e66dae81f4e6
anilkul98 <akul16@ku.edu.tr>
2022-07-17 21:56:18 +0200

    Add user restaurant partner location tabs for login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useContext } from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useNavigate } from 'react-router-dom';
 import { Box, Grid, Typography, TextField, Button } from '@mui/material';
 import { loginUser } from '../queries/user-queries';
 import { AuthUserContext } from '../authentication/AuthUserContext';
+// import { loginPartnerLocation } from '../queries/partner-location-queries';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [partnerType, setPartnerType] = useState('user');
   const authContext = useContext(AuthUserContext);
+
+  const handleChange = (event, newLoginType) => {
+    setPartnerType(newLoginType);
+  };
 
   const navigate = useNavigate();
 
@@ -19,7 +27,7 @@ function LoginPage() {
     setPassword(e.target.value);
   };
 
-  const onSubmitClicked = async () => {
+  const onSubmitClickedUser = async () => {
     try {
       const userData = {
         username,
@@ -36,44 +44,88 @@ function LoginPage() {
     }
   };
 
+  // const onSubmitClickedPartner = async () => {
+  //   try {
+  //     const partnerLocationData = {
+  //       username,
+  //       password,
+  //       partnerType
+  //     };
+  //     const message = await loginPartnerLocation(partnerLocationData);
+  //     const { token } = message;
+  //     authContext.loginUser(token);
+  //     if (token) {
+  //       const partnerData = getDataFromToken(token);
+  //       navigate(`/restaurant-profile/${partnerData.partnerLocation.id}`);
+  //     }
+  //   } catch (e) {
+  //     console.error(`failed to create partner location ${e}`);
+  //   }
+  // };
+
   return (
-    <Box
-      component="form"
-      noValidate
-      sx={{
-        mt: 4,
-        marginLeft: 5,
-        marginRight: 5,
-        marginBottom: 5,
-        minWidth: 400
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
       }}>
-      <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
-        <Grid item>
-          <Typography align="center">Welcome to the Triplan!</Typography>
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          mt: 4,
+          marginLeft: 5,
+          marginRight: 5,
+          marginBottom: 5,
+          minWidth: 400
+        }}>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100vh',
+            margin: 5
+          }}>
+          <ToggleButtonGroup color="primary" value={partnerType} exclusive onChange={handleChange}>
+            <ToggleButton value="user">User</ToggleButton>
+            <ToggleButton value="restaurant">Restaurant</ToggleButton>
+            <ToggleButton value="tourist-attraction">Tourist Attraction</ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+
+        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography align="center">Welcome to the Triplan!</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              defaultValue={username}
+              onChange={onUsernameChanged}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="outlined-required"
+              label="Password"
+              defaultValue={password}
+              onChange={onPasswordChanged}
+            />
+          </Grid>
+          <Grid item>
+            <Button onClick={partnerType === 'user' ? onSubmitClickedUser : onSubmitClickedUser}>
+              Login
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField
-            required
-            id="outlined-required"
-            label="Username"
-            defaultValue={username}
-            onChange={onUsernameChanged}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            required
-            id="outlined-required"
-            label="Password"
-            defaultValue={password}
-            onChange={onPasswordChanged}
-          />
-        </Grid>
-        <Grid item>
-          <Button onClick={onSubmitClicked}>Login</Button>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
   );
 }
 
