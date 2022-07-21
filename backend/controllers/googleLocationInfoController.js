c1f7481c4e9569c9d9fed7e7b657a5f943535833
Batuhan Erden <erden.batuhan@gmail.com>
2022-07-21 12:34:07 +0200

    calculate review score (google) of each location

--- backend/controllers/googleLocationInfoController.js
+++ backend/controllers/googleLocationInfoController.js
@@ -12,4 +12,39 @@ const save = (googleLocationInfo) => {
   );
 };
 
-module.exports = { find, save };
+const getReviewScores = () => {
+  return new Promise((resolve, reject) => {
+    Promise.all([
+      getReviewCountsForPlaces(), getTotalReviewCountsForCities()
+    ])
+      .then(([ reviewCountsForPlaces, totalReviewCountsForCities ]) => {
+        resolve(Object.assign({}, ...Object.entries(reviewCountsForPlaces).map(([_id, { city, review_count }]) => {
+          return { [_id]: review_count / totalReviewCountsForCities[city] }
+        })));
+      })
+      .catch(err => reject(err));
+  });
+};
+
+const getReviewCountsForPlaces = () => {
+  return GoogleLocationInfo.find().select("city review_count").then(response => (
+    Object.assign({}, ...response.map(({ _id, city, review_count }) => (
+      { [_id]: { city, review_count } }
+    )))
+  ));
+};
+
+const getTotalReviewCountsForCities = () => {
+  return GoogleLocationInfo.aggregate([
+    {
+      $group: {
+        _id: "$city",
+        totalReviewCount: {
+          $sum: `$review_count`
+        }
+      }
+    }
+  ]).then(response => Object.assign({}, ...response.map(({ _id, totalReviewCount }) => ({ [_id]: totalReviewCount }))));
+};
+
+module.exports = { find, save, getReviewScores, getReviewCountsForPlaces, getTotalReviewCountsForCities };
