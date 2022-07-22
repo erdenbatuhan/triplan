de53dd4f464e5eb135f8d44d29bcf5d8f2b50037
Batuhan Erden <erden.batuhan@gmail.com>
2022-07-22 23:56:12 +0200

    fix req.body issue & add logging

--- backend/cms/handler.js
+++ backend/cms/handler.js
@@ -5,14 +5,17 @@ const s3 = new AWS.S3();
 
 module.exports.upload = async (event) => {
   const owner = event.queryStringParameters.owner;
-  const encodedImage = event.body.image;
+  const encodedImage = JSON.parse(event.body).image;
 
   if (!owner || !encodedImage) {
+    console.log(`An error occurred with the parameters!\nReceived:\n- Owner: ${owner}\n- Image: ${encodedImage}`);
     return {
       statusCode: 400,
-      body: "Please provide the owner id as a query string and image in the request body!"
+      body: "Please provide the owner as a query string and the image in the request body!"
     };
   }
+
+  console.log(`Uploading an image with length ${encodedImage.length} and assigning it to ${owner}..`);
 
   const imagePath = `${process.env.CMS_STORAGE_BUCKET_DIRECTORY_IMG}/${owner}.png`;
   const uploadPromise = s3.upload({
@@ -22,12 +25,18 @@ module.exports.upload = async (event) => {
     "ContentType": "mime/png"
   }).promise();
 
-  return await uploadPromise.then(() => ({
-    statusCode: 200,
-    body: `${process.env.CMS_STORAGE_BUCKET_URL}/${imagePath}`
-  })).catch(err => ({
-    statusCode: err.statusCode || 400,
-    body: err.message || "Something went wrong when it should not have!"
-  }));
+  return await uploadPromise.then(() => {
+    console.log(`Successfully uploaded the image for ${owner}!`);
+    return {
+      statusCode: 200,
+      body: `${process.env.CMS_STORAGE_BUCKET_URL}/${imagePath}`
+    }
+  }).catch(err => {
+    console.log(`An error occurred while uploading the image for ${owner}!`);
+    return {
+      statusCode: err.statusCode || 400,
+      body: err.message || "Something went wrong when it should not have!"
+    }
+  });
 };
 
