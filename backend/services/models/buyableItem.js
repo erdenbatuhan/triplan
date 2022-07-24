f4a73315a03d0d40f446cb3681cb1053ecbb7c99
EralpKocas <hkocas15@ku.edu.tr>
2022-07-24 16:36:29 +0300

    ItemBought model is created with necessary assocations in BuyableItem and with TripLocation.

--- backend/services/models/buyableItem.js
+++ backend/services/models/buyableItem.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const { Restaurant, TouristAttraction } = require("./partnerLocation.js");
+const { ItemBought } = require("./itemBought.js");
 
 const { extendSchema } = require("./../utils/mongooseUtils.js");
 const enums = require("./../utils/enums.js");
@@ -11,7 +12,14 @@ const BuyableItemSchema = new Schema({
   description: { type: String, default: "", required: true },
   price: { type: Number, required: true },
   appliedDiscountRate: { type: Number, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  associatedItemBoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: ItemBought.name,
+      required: false,
+    },
+  ], // One-to-Many Relation using Reference
 });
 
 const Ticket = mongoose.model(
@@ -19,7 +27,11 @@ const Ticket = mongoose.model(
   extendSchema(BuyableItemSchema, {
     reservationDate: { type: Date, required: true },
     expirationDate: { type: Date, required: true },
-    touristAttraction: { type: Schema.Types.ObjectId, ref: TouristAttraction.name, required: true }, // Many-To-One Relation using Reference
+    touristAttraction: {
+      type: Schema.Types.ObjectId,
+      ref: TouristAttraction.name,
+      required: true,
+    }, // Many-To-One Relation using Reference
   })
 );
 
@@ -27,8 +39,12 @@ const MenuItem = mongoose.model(
   "MenuItem",
   extendSchema(BuyableItemSchema, {
     type: { type: String, enum: enums.FOOD_TYPES, required: true },
-    restaurant: { type: Schema.Types.ObjectId, ref: Restaurant.name, required: true }, // Many-To-One Relation using Reference
+    restaurant: {
+      type: Schema.Types.ObjectId,
+      ref: Restaurant.name,
+      required: true,
+    }, // Many-To-One Relation using Reference
   })
 );
-  
+
 module.exports = { Ticket, MenuItem };
--- backend/services/models/partnerLocation.js
+++ backend/services/models/partnerLocation.js
@@ -9,29 +9,31 @@ const { extendSchema } = require("./../utils/mongooseUtils.js");
 const enums = require("./../utils/enums.js");
 
 const PartnerLocationSchema = new Schema({
-  name: { type: String, default: '', required: false },
+  name: { type: String, default: "", required: false },
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  city: { type: String, default: '', required: true },
-  country: { type: String, default: '', required: true },
-  address: { type: String, default: '', required: false }, 
-  phoneNumber: { type: String, default: '', required: false },
-  googleLocationLink: { type: String, default: '', required: false },
-  certificate: { type: String, default: '', required: false },
-  locationPicture: { type: String, default: '', required: false },
+  city: { type: String, default: "", required: true },
+  country: { type: String, default: "", required: true },
+  address: { type: String, default: "", required: false },
+  phoneNumber: { type: String, default: "", required: false },
+  googleLocationLink: { type: String, default: "", required: false },
+  certificate: { type: String, default: "", required: false },
+  locationPicture: { type: String, default: "", required: false },
   partnerType: {
     type: String,
-    enum : enums.PARTNER_TYPES,
-    required: true
+    enum: enums.PARTNER_TYPES,
+    required: true,
   },
   googleLocationInfo: { type: GoogleLocationInfo.schema, required: false }, // One-to-One Relation using Embedded Documents
-  associatedTripLocations: [{ 
-    type: Schema.Types.ObjectId,
-    ref: TripLocation.name,
-    required: false
-  }], // One-to-Many Relation using Reference
-  wallet: { type: Schema.Types.ObjectId, ref: Wallet.name } // One-to-One Relation using Reference
+  associatedTripLocations: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: TripLocation.name,
+      required: false,
+    },
+  ], // One-to-Many Relation using Reference
+  wallet: { type: Schema.Types.ObjectId, ref: Wallet.name }, // One-to-One Relation using Reference
 });
 
 const Restaurant = mongoose.model(
@@ -39,19 +41,19 @@ const Restaurant = mongoose.model(
   extendSchema(PartnerLocationSchema, {
     cuisines: {
       type: [String],
-      enum : enums.CUISINES,
-      required: false
+      enum: enums.CUISINES,
+      required: false,
     },
     menuList: {
       type: Array,
-      required: false
+      required: false,
     },
     foodTypes: {
       type: [String],
       enum: enums.FOOD_TYPES,
       default: [enums.FOOD_TYPES[0]],
-      required: true
-    }
+      required: true,
+    },
   })
 );
 
@@ -62,8 +64,8 @@ const TouristAttraction = mongoose.model(
       type: [String],
       enum: enums.TOURIST_ATTRACTION_TYPES,
       default: [enums.TOURIST_ATTRACTION_TYPES[0]],
-      required: true
-    }
+      required: true,
+    },
   })
 );
 
