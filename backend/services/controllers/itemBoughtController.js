a4d7298f62cce90b53a42356ab23f1dcc96d0275
EralpKocas <hkocas15@ku.edu.tr>
2022-07-24 17:36:46 +0300

    item bought related queries and routes are implemented.

--- backend/services/controllers/itemBoughtController.js
+++ backend/services/controllers/itemBoughtController.js
@@ -1,9 +1,34 @@
 const { ItemBought } = require("./../models/itemBought.js");
 
-// tripLocationList
-// return: {
-//  tripLocationId: itemsBought
-//  ...
+const addNewItemBoughtEntry = (newItemBought) => {
+  return ItemBought.exists({
+    associatedTripLocation: newItemBought.associatedTripLocation,
+  })
+    .then(() => new Promise(ItemBought.insertMany([newItemBought])))
+    .catch(() => new Promise((resolve) => resolve(null)));
+};
+
+const updateItemBoughtEntry = async (itemBoughtId, fields) => {
+  if (!(await ItemBought.exists({ _id: itemBoughtId }))) {
+    return new Promise((resolve) => resolve(null));
+  }
+  return ItemBought.updateOne({ _id: itemBoughtId }, fields, {
+    new: true,
+    runValidators: true,
+  });
+};
+
+const deleteItemBoughtEntry = (itemBoughtId) => {
+  return ItemBought.findByIdAndDelete(itemBoughtId);
+};
+
+const getItemBoughtEntryById = async (itemBoughtId) => {
+  if (!(await ItemBought.exists({ _id: itemBoughtId }))) {
+    return new Promise((resolve) => resolve(null));
+  }
+
+  return ItemBought.findById(itemBoughtId);
+};
 
 const getItemsBoughtByTripLocations = (tripLocationIdList) => {
   return Promise.all([
@@ -23,5 +48,9 @@ const getItemsBoughtByTripLocations = (tripLocationIdList) => {
 };
 
 module.exports = {
+  addNewItemBoughtEntry,
+  updateItemBoughtEntry,
+  deleteItemBoughtEntry,
+  getItemBoughtEntryById,
   getItemsBoughtByTripLocations,
 };
--- backend/services/routes/itemBoughtRoutes.js
+++ backend/services/routes/itemBoughtRoutes.js
@@ -4,9 +4,90 @@ const router = express.Router();
 const itemBoughtController = require("./../controllers/itemBoughtController.js");
 
 /**
- * Find items bought given a list of trip locations ids.
+ * Adds a new item bought entry for a trip location.
  */
 router.post("/item-bought", async (req, res) => {
+  try {
+    const itemBoughtCreated = await itemBoughtController.addNewItemBoughtEntry(
+      req.body
+    );
+    res.status(200).send(itemBoughtCreated);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(
+        `An error occurred while creating a new item bought entry for a trip location! Error => ${message}`
+      );
+  }
+});
+
+/**
+ * Updates an item bought entry
+ */
+router.put("/item-bought/:id", async (req, res) => {
+  try {
+    const itemBoughtId = req.params.id;
+    const query = req.query;
+
+    const ticketUpdated = await itemBoughtController.updateItemBoughtEntry(
+      itemBoughtId,
+      query
+    );
+    res.status(200).send(ticketUpdated);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(
+        `An error occurred while updating an item bought entry! Error => ${message}`
+      );
+  }
+});
+
+/**
+ * Deletes an existing item bought entry
+ */
+router.delete("/item-bought", async (req, res) => {
+  try {
+    const itemBoughtId = req.query.id;
+    const itemBoughtRemoved = await itemBoughtController.deleteItemBoughtEntry(
+      itemBoughtId
+    );
+
+    if (!itemBoughtRemoved) {
+      return res.status(404).send(`Item Bought not found!`);
+    }
+
+    res.status(200).send(itemBoughtRemoved);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(
+        `An error occurred while deleting the menu item! Error => ${message}`
+      );
+  }
+});
+
+/**
+ * Gets an existing item bought entry given id
+ */
+router.get("/item-bought", async (req, res) => {
+  try {
+    const id = req.query.id;
+    const itemBought = itemBoughtController.getItemBoughtEntryById(id);
+    res.status(200).send(itemBought);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(
+        `An error occurred while getting the item bought for the id! Error => ${message}`
+      );
+  }
+});
+
+/**
+ * Find items bought given a list of trip locations ids.
+ */
+router.post("/", async (req, res) => {
   try {
     const itemsBought = itemBoughtController.getItemsBoughtByTripLocations(
       req.body
