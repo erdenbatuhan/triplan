cadb02b0f141561a2d8895c5f9fbb7798907b59a
EralpKocas <hkocas15@ku.edu.tr>
2022-07-25 14:22:44 +0300

    auth routes are created and index is updated

---
+++ backend/services/routes/authenticationRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+require("dotenv").config();
+
+const authenticationController = require("../controllers/authenticationController.js");
+
+/**
+ * Creates a new user and returns the related user id.
+ */
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
+  try {
+    await authenticationController.signUp(req, res);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(
+        `An error occurred while registering the user! Error => ${message}`
+      );
+  }
+});
+
+/**
+ * Log in a user and get the related user id.
+ */
+router.post("/login", async (req, res) => {
+  try {
+    await authenticationController.login(req, res);
+  } catch ({ message }) {
+    res
+      .status(400)
+      .send(`An error occurred while getting the user! Error => ${message}`);
+  }
+});
+
+module.exports = router;
