1c25c3ab7f7c152530b9d46220cbb043668b3aa1
Batuhan Erden <erden.batuhan@gmail.com>
2022-07-27 21:23:46 +0200

    add missing components

---
+++ frontend/src/components/common/ContentModal.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Grid, Modal, Typography } from '@mui/material';
+
+const modalBoxStyles = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  justifyConent: 'center',
+  alignItems: 'center',
+  borderRadius: 8
+};
+
+export default function ContentModal({ open, onClose, contentStyle, header, contentRendered }) {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box sx={modalBoxStyles}>
+        <Grid sx={contentStyle} alignItems="stretch">
+          {header ? (
+            <Typography
+              sx={{ color: 'text.secondary', fontWeight: 'medium', fontSize: 25, pb: 2 }}
+              align="center">
+              {header}
+            </Typography>
+          ) : (
+            []
+          )}
+
+          {contentRendered}
+        </Grid>
+      </Box>
+    </Modal>
+  );
+}
