e98baa912f989dd2c187d91d7f5b8fb1973fac75
Batuhan Erden <erden.batuhan@gmail.com>
2022-07-27 03:52:32 +0200

    reverted healthCheckRoutes

--- backend/services/routes/healthCheckRoutes.js
+++ backend/services/routes/healthCheckRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const transactionController = require("../controllers/transactionController.js");
+// const myController = require("../controllers/myController.js");
 
 /**
  * Returns if everything is working as expected
@@ -9,7 +9,7 @@ const transactionController = require("../controllers/transactionController.js")
 router.post("/", async (req, res) => {
   try {
     // Replace the following promise with the service of your choice to easily verify that it works
-    const healthCheckPromise = transactionController.buyItems(req.body);
+    const healthCheckPromise = new Promise((resolve) => resolve("Yey! Everything works as expected!"));
     res.status(200).send((await healthCheckPromise));
   } catch ({ message }) {
     res.status(400).send(`An error occurred! Error => ${message}`);
