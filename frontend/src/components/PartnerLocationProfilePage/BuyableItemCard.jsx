d463252bbe1f15d6db84731f251a5936ce010e13
Cansu Yildirim <cansu.yildirim@tum.de>
2022-07-28 07:09:16 +0200

    partner location profile updated

--- frontend/src/components/PartnerLocationProfilePage/BuyableItemCard.jsx
+++ frontend/src/components/PartnerLocationProfilePage/BuyableItemCard.jsx
@@ -5,17 +5,85 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import ListItem from '@mui/material/ListItem';
+// import ListItem from '@mui/material/ListItem';
 import PropTypes from 'prop-types';
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, Avatar } from '@mui/material';
 
 export default function BuyableItemCard(props) {
-  const { itemIdx, name, content, price, image, handleEditClick, handleDeleteClick, inEdit } =
-    props;
+  const {
+    itemIdx,
+    name,
+    content,
+    price,
+    image,
+    handleEditClick,
+    handleDeleteClick /* , inEdit */
+  } = props;
   // const navigate = useNavigate();
   // const { partnerId } = useParams();
 
   return (
+    <Card sx={{ width: '100%', marginBottom: 1 }}>
+      <Grid container direction="column" marginTop={4}>
+        <Grid item xs={8}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            display="inline-flex"
+            mb={2}>
+            <Grid item xs={4} justifyItems="center" display="inline-grid">
+              <Avatar
+                sx={{
+                  width: '125px',
+                  height: '125px'
+                }}
+                src={image}
+              />
+            </Grid>
+            <Grid item xs={8} padding="unset">
+              <CardContent>
+                <Grid container direction="column">
+                  <Grid item xs={6}>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {name}
+                    </Typography>
+
+                    <Typography variant="body2" color="text.secondary">
+                      {content}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={1} sx={{ mb: 2 }} />
+
+                  <Grid item xs={2} justifyContent="right">
+                    <Typography variant="h5" color="text.secondary">
+                      {price} €
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={1} />
+
+                  <Grid item xs={2} justifyContent="inherit">
+                    <Stack direction="row" spacing={2}>
+                      <Button value={itemIdx} onClick={handleEditClick}>
+                        Edit
+                      </Button>
+                      <Button value={itemIdx} onClick={handleDeleteClick}>
+                        Delete
+                      </Button>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+  /* return (
     <Card sx={{ maxWidth: '%100' }}>
       <CardContent>
         <Stack direction="row" spacing={2}>
@@ -65,7 +133,7 @@ export default function BuyableItemCard(props) {
         </Stack>
       </CardContent>
     </Card>
-  );
+  ); */
 }
 
 BuyableItemCard.propTypes = {
@@ -73,11 +141,11 @@ BuyableItemCard.propTypes = {
   name: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  inEdit: PropTypes.bool
+  image: PropTypes.string.isRequired
+  // inEdit: PropTypes.bool
 };
 
 BuyableItemCard.defaultProps = {
-  itemIdx: -1,
-  inEdit: false
+  itemIdx: -1
+  // inEdit: false
 };
--- frontend/src/pages/PartnerLocationProfilePage.jsx
+++ frontend/src/pages/PartnerLocationProfilePage.jsx
@@ -1,30 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams, useLocation } from 'react-router-dom';
-import { Modal, TextField } from '@mui/material';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { useParams, useLocation } from 'react-router-dom';
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  Modal,
+  TextField,
+  Avatar,
+  IconButton,
+  Divider,
+  Paper,
+  List
+} from '@mui/material';
 import { green } from '@mui/material/colors';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
-import ItemListDisplay from '../components/PartnerLocationProfilePage/ItemListDisplay';
-import RestaurantCuisineDisplay from '../components/PartnerLocationProfilePage/RestaurantCuisineDisplay';
-import { getRestaurant, getTouristAttraction } from '../queries/partner-location-queries';
+import PlaceIcon from '@mui/icons-material/Place';
+import PhoneIcon from '@mui/icons-material/Phone';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+// import ItemListDisplay from '../components/PartnerLocationProfilePage/ItemListDisplay';
+// import RestaurantCuisineDisplay from '../components/PartnerLocationProfilePage/RestaurantCuisineDisplay';
+import BuyableItemCard from '../components/PartnerLocationProfilePage/BuyableItemCard';
+import {
+  getRestaurant,
+  getTouristAttraction,
+  saveRestaurant,
+  saveTouristAttraction
+} from '../queries/partner-location-queries';
 import { getMenuItems, getTickets } from '../queries/buyable-item-queries';
-import InfoCard from '../components/InfoCard';
+// import InfoCard from '../components/InfoCard';
 import { createNewPartnerSignupRequest } from '../queries/partner-signup-request-queries';
 import { handleEmail } from '../queries/email-queries';
+import ContentModal from '../components/common/ContentModal';
+import Spinner from '../components/common/Spinner';
 
 // import { modalStyle } from '../shared/styles';
 // import TicketItemDisplay from '../components/RestaurantProfilePage/TicketItemsDisplay';
 import { PARTNER_TYPE_RESTAURANT, PARTNER_TYPE_TOURIST_ATTRACTION } from '../shared/constants';
+// import EditItemModal from '../components/PartnerLocationProfilePage/EditItemModal';
+import EditPartnerLocationCard from '../components/EditPartnerLocationCard';
 
-const mockImgData = {
+/* const mockImgData = {
   img: 'https://fastly.4sqi.net/img/general/width960/41222779_zbo5pj_DAblB24yPU--MnDvDmIlvqIGLuBkc8hZxmyY.jpg',
   title: ''
-};
+}; */
 
 const style = {
   display: 'flex',
@@ -32,9 +53,21 @@ const style = {
   justifyContent: 'center'
 };
 
+const avatarStyle = {
+  width: '200px',
+  height: '200px'
+};
+
 export default function PartnerLocationProfilePage() {
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  // Fetch the restaurant for every change in restaurant ID
+  const { partnerId } = useParams();
+  // const navigate = useNavigate();
+  const partnerLocationType = location.state ? location.state.partnerType : PARTNER_TYPE_RESTAURANT; // tourist-attraction // restaurant
+
   const [partner, setPartner] = useState({});
-  const [cuisineList, setCuisineList] = useState([]);
+  // const [cuisineList, setCuisineList] = useState([]);
   const [menuList, setMenuList] = useState([]);
   const [ticketList, setTicketList] = useState([]);
   const [isConfirmed, setIsConfirmed] = useState('No Request');
@@ -42,14 +75,11 @@ export default function PartnerLocationProfilePage() {
   const [partnerGoogleLink, setPartnerGoogleLink] = useState('');
   const [partnerContactInfo, setPartnerContactInfo] = useState('');
 
-  const location = useLocation();
-  // Fetch the restaurant for every change in restaurant ID
-  const { partnerId } = useParams();
-  const navigate = useNavigate();
-  const partnerLocationType = location.state ? location.state.partnerType : PARTNER_TYPE_RESTAURANT; // tourist-attraction // restaurant
+  const [isPartnerLocationEditMode, setIsPartnerLocationEditMode] = useState(false);
 
   const handleEditClick = () => {
-    navigate(`/edit-partner-profile/${partnerId}`, { state: { partnerType: partnerLocationType } });
+    // navigate(`/edit-partner-profile/${partnerId}`, { state: { partnerType: partnerLocationType } });
+    setIsPartnerLocationEditMode(true);
   };
 
   const handleSendPartnerRequest = () => {
@@ -81,7 +111,7 @@ export default function PartnerLocationProfilePage() {
       getRestaurant(partnerId).then((data) => {
         setPartner(data);
         console.log(data);
-        setCuisineList(data.cuisines);
+        // setCuisineList(data.cuisines);
         setIsConfirmed(data.confirmed);
       });
       getMenuItems(partnerId).then((data) => {
@@ -98,49 +128,166 @@ export default function PartnerLocationProfilePage() {
     }
   }, [partnerId]);
 
+  const handlePartnerFieldsChange = (params) => {
+    const updatedLocation = {
+      _id: partnerId,
+      name: params.partnerName,
+      address: params.partnerAddress,
+      locationPicture: params.partnerLocationPicture
+    };
+
+    setLoading(true);
+
+    if (partnerLocationType === PARTNER_TYPE_RESTAURANT) {
+      saveRestaurant({
+        ...updatedLocation,
+        phoneNumber: params.partnerPhoneNumber,
+        cuisines: params.restaurantCuisines
+      })
+        .then(() => getRestaurant(partnerId).then((data) => setPartner(data)))
+        .finally(() => setLoading(false));
+    } else if (partnerLocationType === PARTNER_TYPE_TOURIST_ATTRACTION) {
+      saveTouristAttraction(updatedLocation)
+        .then(() => getTouristAttraction(partnerId).then((data) => setPartner(data)))
+        .finally(() => setLoading(false));
+    }
+  };
+
+  if (loading) {
+    return <Spinner marginTop="5em" />;
+  }
+
   return (
-    <Box
-      component="form"
-      noValidate
-      sx={{
-        mt: 4,
-        marginLeft: 5,
-        marginRight: 5,
-        marginBottom: 5,
-        minWidth: 400
-      }}>
-      <Grid container direction="row" spacing={8}>
-        <Grid item xs={3}>
-          <Stack spacing={4}>
-            <img
-              src={`${partner.locationPicture}?w=164&h=164&fit=crop&auto=format`}
-              srcSet={`${partner.locationPicture}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-              alt={mockImgData.title}
-              loading="lazy"
-              width={250}
-              height={250}
-            />
-            <InfoCard title="Address" value={partner.address} />
-            <InfoCard title="Phone Number" value={partner.phoneNumber} />
-            <Button variant="contained" onClick={handleEditClick}>
-              Edit Profile
-            </Button>
-          </Stack>
-        </Grid>
-        <Grid item xs={9}>
-          <Stack>
-            <Typography variant="h1" component="div" align="center">
-              {partner.name}
+    <Grid container spacing={2} m={5}>
+      <Grid item xs={3}>
+        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={1}>
+          <Grid item xs={6}>
+            <Avatar sx={avatarStyle} src={partner.locationPicture} loading="lazy" />
+          </Grid>
+
+          <Grid item xs={3} display="flex">
+            <IconButton pointerEvents="none">
+              <PlaceIcon />
+            </IconButton>
+
+            <Typography component="div" align="center" m={1} sx={{ fontSize: 'subtitle1' }}>
+              {partner.address}
             </Typography>
-            {partnerLocationType === 'restaurant' ? (
-              <Stack>
-                <RestaurantCuisineDisplay cuisineList={cuisineList} />
-                <ItemListDisplay itemList={menuList} inEdit={false} />
-              </Stack>
-            ) : (
-              <ItemListDisplay itemList={ticketList} inEdit={false} />
-            )}
-          </Stack>
+          </Grid>
+
+          <Grid item xs={3} display="flex">
+            <IconButton pointerEvents="none">
+              <PhoneIcon />
+            </IconButton>
+
+            <Typography component="div" align="center" m={1} sx={{ fontSize: 'subtitle1' }}>
+              {partner.phoneNumber}
+            </Typography>
+          </Grid>
+
+          {partner.cuisines ? (
+            <Grid item display="flex">
+              <IconButton pointerEvents="none">
+                <RestaurantIcon />
+              </IconButton>
+
+              <Typography
+                component="div"
+                sx={{ fontSize: 'subtitle1', color: 'text.secondary', pt: 1 }}>
+                {partner.cuisines.join(' ')}
+              </Typography>
+            </Grid>
+          ) : (
+            <Grid item />
+          )}
+
+          {partner.foodTypes ? (
+            <Grid item display="flex">
+              <IconButton pointerEvents="none">
+                <MenuBookIcon />
+              </IconButton>
+
+              <Typography
+                component="div"
+                sx={{ fontSize: 'subtitle1', color: 'text.secondary', pt: 1 }}>
+                {partner.foodTypes.join(' ')}
+              </Typography>
+            </Grid>
+          ) : (
+            <Grid item />
+          )}
+        </Grid>
+
+        <Grid item display="grid" sx={{ m: 2 }}>
+          <Button variant="contained" onClick={handleEditClick}>
+            Edit Profile
+          </Button>
+        </Grid>
+      </Grid>
+
+      <ContentModal
+        open={isPartnerLocationEditMode}
+        onClose={() => {
+          setIsPartnerLocationEditMode(false);
+        }}
+        contentStyle={{ minWidth: '500px' }}
+        header="Edit Profile"
+        contentRendered={
+          <EditPartnerLocationCard
+            partner={partner}
+            handlePartnerFieldsChange={handlePartnerFieldsChange}
+          />
+        }
+      />
+
+      <Grid item xs={1} />
+
+      <Grid item xs={6}>
+        <Grid>
+          <Typography variant="h2" component="div" align="left" color="text.secondary">
+            {partner.name}
+          </Typography>
+
+          <Divider />
+        </Grid>
+        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={4}>
+          {/* <Grid item xs={9} sx={{ width: '100%' }}>
+            <Stack>
+              {partnerLocationType === 'restaurant' ? (
+                <Stack>
+                  <RestaurantCuisineDisplay cuisineList={cuisineList} />
+                  <ItemListDisplay itemList={menuList} inEdit={false} />
+                </Stack>
+              ) : (
+                <ItemListDisplay itemList={ticketList} inEdit={false} />
+              )}
+            </Stack>
+              </Grid> */}
+          <Grid item xs={9} sx={{ width: '100%' }}>
+            <Paper style={{ maxHeight: 500, overflow: 'auto' }}>
+              <List spacing={2} overflow="auto">
+                {(partnerLocationType === PARTNER_TYPE_RESTAURANT
+                  ? menuList || []
+                  : ticketList || []
+                ).map((item, idx) => {
+                  return (
+                    <BuyableItemCard
+                      key={item._id}
+                      menuId={item._id}
+                      itemIdx={idx}
+                      name={item.name}
+                      content={item.description}
+                      price={item.price.toString()}
+                      image={item.image}
+                      handleEditClick={handleEditClick}
+                      // handleDeleteClick={handleDeleteClick}
+                      inEdit
+                    />
+                  );
+                })}
+              </List>
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
 
@@ -223,6 +370,134 @@ export default function PartnerLocationProfilePage() {
           </div>
         </Box>
       </Modal>
-    </Box>
+    </Grid>
   );
+
+  /* return (
+    <Box
+      component="form"
+      noValidate
+      sx={{
+        mt: 4,
+        marginLeft: 5,
+        marginRight: 5,
+        marginBottom: 5,
+        minWidth: 400
+      }}>
+      <Grid container direction="row" spacing={8}>
+        <Grid item xs={3}>
+          <Stack spacing={4}>
+            <img
+              src={`${partner.locationPicture}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${partner.locationPicture}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={mockImgData.title}
+              loading="lazy"
+              width={250}
+              height={250}
+            />
+            <InfoCard title="Address" value={partner.address} />
+            <InfoCard title="Phone Number" value={partner.phoneNumber} />
+            <Button variant="contained" onClick={handleEditClick}>
+              Edit Profile
+            </Button>
+          </Stack>
+        </Grid>
+        <Grid item xs={9}>
+          <Stack>
+            <Typography variant="h1" component="div" align="center">
+              {partner.name}
+            </Typography>
+            {partnerLocationType === 'restaurant' ? (
+              <Stack>
+                <RestaurantCuisineDisplay cuisineList={cuisineList} />
+                <ItemListDisplay itemList={menuList} inEdit={false} />
+              </Stack>
+            ) : (
+              <ItemListDisplay itemList={ticketList} inEdit={false} />
+            )}
+          </Stack>
+        </Grid>
+      </Grid> 
+
+      <Modal
+        open={isConfirmed === 'No Request'}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box style={style}>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            spacing={2}
+            minWidth="60ch">
+            <Typography variant="h6" component="h2" color="text.secondary">
+              Please Send Confirmation Request!
+            </Typography>
+
+            <Grid item>
+              <TextField
+                id="standard-basic"
+                label="Please enter the Google Maps Link of the location"
+                variant="standard"
+                value={partnerGoogleLink}
+                onChange={(event) => setPartnerGoogleLink(event.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="standard-basic"
+                label="Please enter the name of your business."
+                variant="standard"
+                value={partnerName}
+                onChange={(event) => setPartnerName(event.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="standard-basic"
+                label="Please enter a contact number."
+                variant="standard"
+                value={partnerContactInfo}
+                onChange={(event) => setPartnerContactInfo(event.target.value)}
+              />
+            </Grid>
+            <br />
+            <Button
+              style={{
+                color: '#FFFFFF',
+                backgroundColor: green[500],
+                width: '60%',
+                border: 1,
+                // borderColor: grey[500],
+                borderRadius: 4,
+                height: '60px'
+              }}
+              onClick={handleSendPartnerRequest}>
+              Send Withdraw Request
+            </Button>
+          </Grid>
+        </Box>
+      </Modal>
+      <Modal
+        open={isConfirmed === 'Requested'}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{
+          display: 'flex',
+          justifyConent: 'center',
+          alignItems: 'center'
+        }}>
+        <Box sx={style}>
+          <div className="center">
+            <Alert severity="info">
+              <AlertTitle>Your request is processing!</AlertTitle>
+              Your request is still under investigation. We will get in touch with you as soon as
+              possible.
+            </Alert>
+          </div>
+        </Box>
+      </Modal>
+    </Box>
+  ); */
 }
